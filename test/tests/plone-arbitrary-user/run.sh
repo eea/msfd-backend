6b5a6d2ec161483d9ed7bff36dfe2ede2b060244
valentinab25 <@users.noreply.github.com>
2026-04-01 18:52:18 +0300

    chore: [JENKINSFILE] add automated nightly release

--- test/tests/plone-arbitrary-user/run.sh
+++ test/tests/plone-arbitrary-user/run.sh
@@ -8,21 +8,15 @@ image="$1"
 PLONE_TEST_SLEEP=5
 PLONE_TEST_TRIES=20
 
-# Start ZEO server
-zname="zeo-container-$RANDOM-$RANDOM"
-zpull="$(docker pull plone/plone-zeo)"
-zid="$(docker run -d --name "$zname" plone/plone-zeo)"
-
-# Start Plone as ZEO Client
-pname="plone-container-$RANDOM-$RANDOM"
-pid="$(docker run -d --name "$pname" --link=$zname:zeo -e ZEO_ADDRESS=zeo:8100 "$image")"
-
-# Tear down
-trap "docker rm -vf $pid $zid > /dev/null" EXIT
+vname="/tmp/plone-data-$RANDOM-$RANDOM"
+cname="plone-container-$RANDOM-$RANDOM"
+vid="$(mkdir -p $vname)"
+cid="$(docker run -d --user=$(id -u) -v $vname:/data --name $cname $image)"
+trap "docker rm -vf $cid > /dev/null; rm -rf $vname > /dev/null" EXIT
 
 get() {
 	docker run --rm -i \
-		--link "$pname":plone \
+		--link "$cname":plone \
 		--entrypoint /app/bin/python \
 		"$image" \
 		-c "from urllib.request import urlopen; con = urlopen('$1'); print(con.read())"
@@ -30,11 +24,12 @@ get() {
 
 get_auth() {
 	docker run --rm -i \
-		--link "$pname":plone \
+		--link "$cname":plone \
 		--entrypoint /app/bin/python \
 		"$image" \
 		-c "from urllib.request import urlopen, Request; request = Request('$1'); request.add_header('Authorization', 'Basic $2'); print(urlopen(request).read())"
 }
+
 
 . "$dir/../../retry.sh" --tries "$PLONE_TEST_TRIES" --sleep "$PLONE_TEST_SLEEP" get "http://plone:8080"
 
@@ -42,4 +37,4 @@ get_auth() {
 [[ "$(get 'http://plone:8080')" == *"Welcome to Plone!"* ]]
 
 # Create a Plone site
-#[[ "$(get_auth 'http://plone:8080/@@ploneAddSite?distribution=classic' "$(echo -n 'admin:admin' | base64)")" == *"Create a Plone site (Classic UI)"* ]]
+#[[ "$(get_auth 'http://plone:8080/@@ploneAddSite?distribution=classic' "$(echo -n 'admin:admin' | base64)")" == *"plone-overview.min.js"* ]]
--- test/tests/plone-shared-blob-dir/run.sh
+++ test/tests/plone-shared-blob-dir/run.sh
@@ -5,24 +5,24 @@ dir="$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
 image="$1"
 
-PLONE_TEST_SLEEP=5
-PLONE_TEST_TRIES=20
+PLONE_TEST_SLEEP=10
+PLONE_TEST_TRIES=10
 
 # Start ZEO server
 zname="zeo-container-$RANDOM-$RANDOM"
 zpull="$(docker pull plone/plone-zeo)"
-zid="$(docker run -d --name "$zname" plone/plone-zeo)"
+zid="$(docker run -d -v $zname:/data --name "$zname" plone/plone-zeo)"
 
 # Start Plone as ZEO Client
 pname="plone-container-$RANDOM-$RANDOM"
-pid="$(docker run -d --name "$pname" --link=$zname:zeo -e ZEO_ADDRESS=zeo:8100 "$image")"
+pid="$(docker run -d -v $zname:/data -e ZEO_SHARED_BLOB_DIR=on --name "${pname}" --link=$zname:zeo -e ZEO_ADDRESS=zeo:8100 "$image")"
 
 # Tear down
 trap "docker rm -vf $pid $zid > /dev/null" EXIT
 
 get() {
 	docker run --rm -i \
-		--link "$pname":plone \
+		--link "${pname}":plone \
 		--entrypoint /app/bin/python \
 		"$image" \
 		-c "from urllib.request import urlopen; con = urlopen('$1'); print(con.read())"
@@ -30,7 +30,7 @@ get() {
 
 get_auth() {
 	docker run --rm -i \
-		--link "$pname":plone \
+		--link "${pname}":plone \
 		--entrypoint /app/bin/python \
 		"$image" \
 		-c "from urllib.request import urlopen, Request; request = Request('$1'); request.add_header('Authorization', 'Basic $2'); print(urlopen(request).read())"
@@ -39,7 +39,7 @@ get_auth() {
 . "$dir/../../retry.sh" --tries "$PLONE_TEST_TRIES" --sleep "$PLONE_TEST_SLEEP" get "http://plone:8080"
 
 # Plone is up and running
-[[ "$(get 'http://plone:8080')" == *"Welcome to Plone!"* ]]
+[[ "$(get 'http://plone:8080')" == *"Plone is up and running"* ]]
 
 # Create a Plone site
-#[[ "$(get_auth 'http://plone:8080/@@ploneAddSite?distribution=classic' "$(echo -n 'admin:admin' | base64)")" == *"Create a Plone site (Classic UI)"* ]]
+[[ "$(get_auth 'http://plone:8080/@@plone-addsite' "$(echo -n 'admin:admin' | base64)")" == *"Create a Plone site"* ]]
